cdaefd91ce7c98002ff5b1131faf70cbd9a4e514
Gabe Comer <gabriel.comer@mavs.uta.edu>
2016-10-13 02:35:51 -0500

    Test file now shows getting and setting of various components of parts, and allows creation of robot part objects

--- Makefile
+++ Makefile
@@ -7,11 +7,13 @@ debug: CXXFLAGS += -g
 debug: executable
 
 rebuild: clean executable
-executable: Arms.o Part.o Battery.o
-	$(CXX) $(CXXFLAGS) Arms.o Part.o Battery.o
+executable: Arms.o Part.o Battery.o test.o
+	$(CXX) $(CXXFLAGS) Arms.o Part.o Battery.o test.o
 Part.o: Part.cpp
 	$(CXX) $(CXXFLAGS) -w -c Part.cpp
 Battery.o: Battery.cpp
 	$(CXX) $(CXXFLAGS) -w -c Battery.cpp
+test.o: test.cpp
+	$(CXX) $(CXXFLAGS) -w -c test.cpp
 clean:
 	rm -f *.o a.out
--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include "Part.h"
+#include "Battery.h"
+#include "Arms.h"
 
-void main(){
+int main(){
 
+    Battery test_bat = Battery(4, 10, 42);
     Arms test_arm = Arms(2, 3);
     int x;
+    string name;
 
     x = test_arm.getType();
     cout << "arm type: " << x << "\n";
@@ -12,5 +16,14 @@ void main(){
     x = test_arm.getSpeed();
     cout << "arm speed: " << x << "\n";
 
-    
+    x = test_bat.getCurrentCharge();
+    cout << "current battery charge: " << x << "\n";
+
+    x = test_bat.getMaxCharge();
+    cout << "max battery charge: " << x << "\n";
+
+    test_bat.setName("Mr. Test");
+    cout << "the battery is named: " << test_bat.getName() << "\n";
+
+    return 1; 
 }
